e5bc741952a7c7d700d606c05e91f073d5d98ce5
ParisKitty <zhichuan.jin.paris@gmail.com>
2021-05-30 13:06:13 +0200

    refactoring

--- csharp/Tennis/TennisGame2.cs
+++ csharp/Tennis/TennisGame2.cs
@@ -6,14 +6,7 @@ namespace Tennis
     public class TennisGame2 : ITennisGame
     {
         private Players players;
-        // delegates for different cases
-        private Predicate<Players> isEqualsWithLessThan3Points = delegate (Players players) { return players.HasSamePoints() && !players.BothHasAtLeast3Points(); };
-        private Predicate<Players> isEqualsWithAtLeast3Points = delegate (Players players) { return players.HasSamePoints() && players.BothHasAtLeast3Points(); };
-        private Predicate<Players> isPlayer1InAdvantage = delegate (Players players) { return players.Player1HasMorePoints() && players.BothHasAtLeast3Points(); };
-        private Predicate<Players> isPlayer2InAdvantage = delegate (Players players) { return players.Player2HasMorePoints() && players.BothHasAtLeast3Points(); };
-        private Predicate<Players> isPLayer1Winner = delegate (Players players) { return players.Player1.HasAtLeast4Points() && players.Player1.HasAtLeast2PointsMoreThan(players.Player2); };
-        private Predicate<Players> isPLayer2Winner = delegate (Players players) { return players.Player2.HasAtLeast4Points() && players.Player2.HasAtLeast2PointsMoreThan(players.Player1); };
-
+       
         public TennisGame2(string player1Name, string player2Name)
         {
             var player1 = new Player(player1Name);
@@ -23,12 +16,12 @@ namespace Tennis
 
         public string GetScore()
         {
-            if (isPLayer1Winner(players)) return TennisConstant.WIN_FOR_PLAYER1;
-            if (isPLayer2Winner(players)) return TennisConstant.WIN_FOR_PLAYER2;
-            if (isEqualsWithAtLeast3Points(players)) return TennisConstant.DEUCE; 
-            if (isEqualsWithLessThan3Points(players)) return players.Player1.GetTennisScore() + TennisConstant.ALL;
-            if (isPlayer1InAdvantage(players)) return TennisConstant.ADVANTAGE_PLAYER_1;
-            if (isPlayer2InAdvantage(players)) return TennisConstant.ADVANTAGE_PLAYER_2;
+            if (Rules.isPLayer1Winner(players)) return TennisConstant.WIN_FOR_PLAYER1;
+            if (Rules.isPLayer2Winner(players)) return TennisConstant.WIN_FOR_PLAYER2;
+            if (Rules.isEqualsWithAtLeast3Points(players)) return TennisConstant.DEUCE; 
+            if (Rules.isEqualsWithLessThan3Points(players)) return players.Player1.GetTennisScore() + TennisConstant.ALL;
+            if (Rules.isPlayer1InAdvantage(players)) return TennisConstant.ADVANTAGE_PLAYER_1;
+            if (Rules.isPlayer2InAdvantage(players)) return TennisConstant.ADVANTAGE_PLAYER_2;
             return players.Player1.GetTennisScore() + TennisConstant.TO + players.Player2.GetTennisScore();
         }
 
